1c9a233a39b2f8ffd8e1cfaa2f321c74a2eef743
Jason Hazel <me@jasonhazel.com>
2014-02-26 15:49:52 -0500

    cleaning up some stuff

--- v1/classes/exceptions.php
+++ v1/classes/exceptions.php
@@ -8,22 +8,22 @@
  */
 
 class RestServiceException extends \Exception {
-	
-	/**
-	 * Convert exception to JSON string
-	 * @return string exception details as JSON string
-	 */
-	function __toString() {
-		$data = array(
-			"error" => array(
-			   "message" => $this->message,
-			   "type" => get_class($this),
-			   "code" => $this->code
-			)
-		 );
-		return json_encode($data,JSON_PRETTY_PRINT);
-	}
-	
+    
+    /**
+     * Convert exception to JSON string
+     * @return string exception details as JSON string
+     */
+    function __toString() {
+        $data = array(
+            "error" => array(
+               "message" => $this->message,
+               "type" => get_class($this),
+               "code" => $this->code
+            )
+         );
+        return json_encode($data,JSON_PRETTY_PRINT);
+    }
+    
 }
 
 ?>
--- v1/classes/url.php
+++ v1/classes/url.php
@@ -9,97 +9,92 @@ namespace classes;
 
 class url {
 
-	private $scheme;
-	private $private;
-	private $host;
-	private $port;
-	private $user;
-	private $pass;
-	private $basePath;
-	private $subPath;
-	private $path;
-	private $query;
-	private $fragment;
+    private $scheme;
+    private $private;
+    private $host;
+    private $port;
+    private $user;
+    private $pass;
+    private $basePath;
+    private $subPath;
+    private $path;
+    private $query;
+    private $fragment;
 
-	function __construct() {
-		
-	}
+    function __construct() {
+        
+    }
 
-	function fromContext() {
-		if ($_SERVER["HTTPS"] == "on") {
-			$scheme = "https";
-		} else {
-			$scheme = "http";
-		}
-		$host = $_SERVER["SERVER_NAME"];
-		$port = $_SERVER["SERVER_PORT"];
-		$uri = $_SERVER["REQUEST_URI"];
+    function fromContext() {
+        $scheme = ($_SERVER['HTTPS'] == 'on' ? 'https' : 'http');
 
-		$url = $scheme . "://" . $host . ":" . $port . $uri;
+        $host = $_SERVER["SERVER_NAME"];
+        $port = $_SERVER["SERVER_PORT"];
+        $uri = $_SERVER["REQUEST_URI"];
 
-		$url = parse_url($url);
+        $url = $scheme . "://" . $host . ":" . $port . $uri;
 
-		foreach ($url as $name => $value) {
-			$this->{$name} = $value;
-		}
+        $url = parse_url($url);
 
-		$this->basePath = dirname($_SERVER["SCRIPT_NAME"]);
+        foreach ($url as $name => $value) {
+            $this->{$name} = $value;
+        }
 
-		$this->basePath = dirname($_SERVER["SCRIPT_NAME"]);
-		$this->subPath = dirname($_SERVER["REQUEST_URI"]);
+        $this->basePath = dirname($_SERVER["SCRIPT_NAME"]);
+        $this->subPath  = dirname($_SERVER["REQUEST_URI"]);
 
-		$this->subPath = preg_replace("/^" . addcslashes(preg_quote($this->basePath), "/") . "/", "", $this->path);
+        $this->subPath = preg_replace("/^" . addcslashes(preg_quote($this->basePath), "/") . "/", "", $this->path);
 
-		parse_str($this->query, $this->query);
-		return $this;
-	}
+        parse_str($this->query, $this->query);
+        return $this;
+    }
 
-	function replica() {
-		$tmp = clone $this;
-		return $tmp;
-	}
+    function replica() {
+        $tmp = clone $this;
+        return $tmp;
+    }
 
-	function __toString() {
-		http_build_query($this->query);
-		return $this->scheme . "://" . $this->host . ":" . $this->port . $this->basePath . $this->subPath . "?" . $this->queryString();
-	}
+    function __toString() {
+        http_build_query($this->query);
+        return $this->scheme . "://" . $this->host . ":" . $this->port . $this->basePath . $this->subPath . "?" . $this->queryString();
+    }
 
-	function addString($str) {
-		parse_str($str, $tmp);
-		$this->query = array_merge($this->query, $tmp);
-		return $this;
-	}
+    function addString($str) {
+        parse_str($str, $tmp);
+        $this->query = array_merge($this->query, $tmp);
+        return $this;
+    }
 
-	function queryString() {
-		$tmp = $this->_queryString();
-		return implode("&", $tmp);
-	}
+    function queryString() {
+        $tmp = $this->_queryString();
+        return implode("&", $tmp);
+    }
 
-	function _queryString($value = null, $key = null, $opt = array()) {
-		static $path = array();
-		static $tmp = array();
+    function _queryString($value = null, $key = null, $opt = array()) {
+        static $path = array();
+        static $tmp = array();
 
-		if (isset($opt["on"]))
-			array_push($path, count($path) ? "[" . ($opt["assoc"] ? $key : "") . "]" : $key);
-		else {
-			$value = $this->query;
-			$path = array();
-			$tmp = array();
-		}
+        if (isset($opt["on"]))
+            array_push($path, count($path) ? "[" . ($opt["assoc"] ? $key : "") . "]" : $key);
+        else {
+            $value = $this->query;
+            $path = array();
+            $tmp = array();
+        }
 
-		if (is_array($value) || !isset($opt["on"])) {
-			$opt["on"] = true;
-			$opt["assoc"] = array_keys($value) !== range(0, count($value) - 1);
-			array_walk($value, array($this, '_queryString'), $opt);
-		} else {
-			$tmp[] = implode("", $path) . "=" . $value;
-		}
+        if (is_array($value) || !isset($opt["on"])) {
+            $opt["on"] = true;
+            $opt["assoc"] = array_keys($value) !== range(0, count($value) - 1);
+            array_walk($value, array($this, '_queryString'), $opt);
+        } else {
+            $tmp[] = implode("", $path) . "=" . $value;
+        }
 
-		if (isset($opt["on"]))
-			array_pop($path);
+        if (isset($opt["on"]))
+            array_pop($path);
 
-		return $tmp;
-	}
+        return $tmp;
+    }
 
 }
 
